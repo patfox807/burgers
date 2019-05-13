080db176d7301f1f98be766be4d40a0759bf3f5e
patfox807 <47128021+patfox807@users.noreply.github.com>
2019-05-13 13:54:02 -0400

    update heroku

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS burgers_db;
+
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
@@ -5,6 +7,6 @@ USE burgers_db;
 CREATE TABLE burgers(
 id INT NOT NULL AUTO_INCREMENT,
 burger_name VARCHAR(200) NOT NULL,
-devoured TINYINT(50) NOT NULL,
+devoured BOOLEAN NOT NULL default false,
 PRIMARY KEY (id)
 );
